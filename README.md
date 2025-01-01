fc4edaf607ef438dbc6276254b5ff3ee570cfa6d
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-01-01 01:48:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Frameworks
+ AN experimental framework collection for building production apps, websites and general tooling for developers
---
+++ flative/platforms/ios/flative/flative/flativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  flativeApp.swift
+//  flative
+//
+//  Created by Tahiru Agbanwa on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct flativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
